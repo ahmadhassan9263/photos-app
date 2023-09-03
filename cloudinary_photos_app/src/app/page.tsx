070ece06d5ff9d8e0a8fd48925941e2abf5e67db
Ahmad Hassan <109802748+ahmadhassan9263@users.noreply.github.com>
2023-09-03 23:52:04 +0500

    cloudinary photo app

---
+++ cloudinary_photos_app/src/app/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { CldUploadButton } from "next-cloudinary";
+import { CldImage } from "next-cloudinary";
+import { useState } from "react";
+
+export type UploadResult = {
+  event: "success";
+  info: {
+    public_id: string;
+  };
+};
+
+export default function Home() {
+  const [imageId, setImageId] = useState("cld-sample-2");
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <CldUploadButton
+        onUpload={(result: UploadResult) => {
+          setImageId(result.info.public_id);
+        }}
+        uploadPreset="gdezsemo"
+      />
+      {imageId && (
+        <CldImage
+          width="400"
+          height="300"
+          src={imageId}
+          sizes="100vw"
+          alt="Description of my image"
+        />
+      )}
+    </main>
+  );
+}
